efe0d8e14fcf058d8e962efb21ab756901776e06
Joachim Viide <jviide@iki.fi>
2021-12-20 21:17:58 +0200

    Modify name.decode to throw an error in the following cases:  * Not enough data for reading the full label  * The label is too long (over 253 characters when dots are included)   * A label must be either <= 63 bytes or a pointer   * Pointers can only point to prior data (see RFC 1035, section 4.1.4)
    In addition pointer jumps don't add extra dots in the names anymore.

--- index.js
+++ index.js
@@ -42,37 +42,52 @@ name.encode = function (str, buf, offset) {
 
 name.encode.bytes = 0
 
+function decodeName (buf, offset, list, totalLength) {
+  const oldOffset = offset
+  let len = buf[offset++]
+
+  while (len > 0) {
+    if ((len & 0xc0) === 0xc0) {
+      if (offset + 1 > buf.length) {
+        throw new Error('Can\'t decode name (buffer overflow)')
+      }
+      const jumpOffset = buf.readUInt16BE(offset - 1) - 0xc000
+      if (jumpOffset >= oldOffset) {
+        // Allow only pointers to prior data. RFC 1035, section 4.1.4 states:
+        // "[...] an entire domain name or a list of labels at the end of a domain name
+        // is replaced with a pointer to a prior occurance (sic) of the same name."
+        throw new Error('Can\'t decode name (bad pointer)')
+      }
+      decodeName(buf, jumpOffset, list, totalLength)
+      offset++
+      break
+    } else if ((len & 0xc0) === 0) {
+      if (offset + len > buf.length) {
+        throw new Error('Can\'t decode name (buffer overflow)')
+      }
+      totalLength += len + 1
+      if (totalLength > 254) {
+        throw new Error('Can\'t decode name (name too long)')
+      }
+      list.push(buf.toString('utf-8', offset, offset + len))
+      offset += len
+      len = buf[offset++]
+    } else {
+      throw new Error('Can\'t decode name (bad label)')
+    }
+  }
+
+  return offset
+}
+
 name.decode = function (buf, offset) {
   if (!offset) offset = 0
 
   const list = []
-  const oldOffset = offset
-  let len = buf[offset++]
+  const newOffset = decodeName(buf, offset, list, 0)
 
-  if (len === 0) {
-    name.decode.bytes = 1
-    return '.'
-  }
-  if (len >= 0xc0) {
-    const res = name.decode(buf, buf.readUInt16BE(offset - 1) - 0xc000)
-    name.decode.bytes = 2
-    return res
-  }
-
-  while (len) {
-    if (len >= 0xc0) {
-      list.push(name.decode(buf, buf.readUInt16BE(offset - 1) - 0xc000))
-      offset++
-      break
-    }
-
-    list.push(buf.toString('utf-8', offset, offset + len))
-    offset += len
-    len = buf[offset++]
-  }
-
-  name.decode.bytes = offset - oldOffset
-  return list.join('.')
+  name.decode.bytes = newOffset - offset
+  return list.length === 0 ? '.' : list.join('.')
 }
 
 name.decode.bytes = 0
